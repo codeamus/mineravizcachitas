7f3c07770a71393daaee832a6782a42653a06100
Alexander Urrutia <matias04041994@gmail.com>
2024-01-08 18:24:16 -0300

    Se integra back to top

---
+++ src/components/molecules/BackToTop/index.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+const BackToTop = () => {
+  return (
+    <a id='back-top' className='mt-' href='#top'>
+      Volver
+    </a>
+  )
+}
+
+export default BackToTop
--- src/components/templates/TemplateHome.tsx
+++ src/components/templates/TemplateHome.tsx
@@ -1,11 +1,83 @@
 import { Link } from 'react-router-dom'
+import BackToTop from '../molecules/BackToTop'
 const TemplateHome = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Home</h1>
-      <Link to='/blog' unstable_viewTransition>
-        Blog
-      </Link>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+        <h1 className='text-3xl font-bold underline'>Home</h1>
+        <Link to='/blog' unstable_viewTransition>
+          Blog
+        </Link>
+      </div>
+      <BackToTop />
     </div>
   )
 }
