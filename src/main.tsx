00408a0862f93bfcf8c07a5586a130f34275afc8
Alexander Urrutia <matias04041994@gmail.com>
2024-02-01 15:15:23 -0300

    remove storybook

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Route from './routes'
-
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
