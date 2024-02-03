b1af353b7c6377567d7b0f44351cae032567d639
Alexander Urrutia <matias04041994@gmail.com>
2024-02-03 12:50:07 -0300

    add million js and react helmet

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import million from 'million/compiler'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src/'),
