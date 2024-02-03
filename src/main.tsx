d4059ad71b1a3e8a75b23fad9f73f0393c7d8350
Alexander Urrutia <matias04041994@gmail.com>
2024-02-03 13:16:41 -0300

    add provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
+import { HelmetProvider } from 'react-helmet-async'
 import Route from './routes'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <Route />
+      <HelmetProvider>
+        <Route />
+      </HelmetProvider>
     </NextUIProvider>
   </React.StrictMode>
 )
