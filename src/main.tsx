d744fbb3641826a12bfa9adf582c3af65f80f6ae
Alexander Urrutia <matias04041994@gmail.com>
2024-02-04 17:26:14 -0300

    delete nextui

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { NextUIProvider } from '@nextui-org/react'
 import { HelmetProvider } from 'react-helmet-async'
 import Route from './routes'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <HelmetProvider>
-        <Route />
-      </HelmetProvider>
-    </NextUIProvider>
+    <HelmetProvider>
+      <Route />
+    </HelmetProvider>
   </React.StrictMode>
 )
