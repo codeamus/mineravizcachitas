e9c71cb48e83457a772fce0f9eeccaed84270850
Alexander Urrutia <matias04041994@gmail.com>
2024-02-09 21:22:56 -0300

    Se reemplaza por componente boton default reutilizable

--- src/components/organism/Home/Compromisos.tsx
+++ src/components/organism/Home/Compromisos.tsx
@@ -1,11 +1,10 @@
 import BtnScrollDown from '@/components/molecules/BtnScrollDown'
-import { Link } from 'react-router-dom'
 
-import IconArrow from '@/assets/icons/arrow-right-bold.svg'
 import IconAmbiental from '@/assets/icons/icon-ambiental-white.svg'
 import IconSocial from '@/assets/icons/icon-social-white.svg'
 import ImageAmbiental from '@/assets/images/compromiso-ambiental.webp'
 import ImageResponsabilidad from '@/assets/images/compromiso-responsabilidad.webp'
+import DefaultButton from '@/components/molecules/Buttons/DefaultButton'
 
 const Compromisos = () => {
   return (
@@ -74,18 +73,13 @@ const Compromisos = () => {
         </article>
       </div>
       <div className='relative -bottom-5 flex justify-center'>
-        <Link
-          to='sostenibilidad'
-          unstable_viewTransition
-          className='flex w-fit flex-row items-center justify-between bg-[#E8732D] px-3 text-center text-sm transition-all duration-300 hover:bg-[#03773A]'
-        >
-          <span>
-            Ver todos los compromisos <br />
-            sostenibles
-          </span>
-          <hr className='mx-4 h-[50px] w-[1px] bg-white' />
-          <img src={IconArrow} alt='' className='w-[16px]' />
-        </Link>
+        <DefaultButton
+          url='/sostenibilidad'
+          textFirst='Ver todos los compromisos'
+          textSecond='sostenibles'
+          backgroundColor='#E8732D'
+          hoverBackgroundColor='#03773A'
+        />
       </div>
     </section>
   )
