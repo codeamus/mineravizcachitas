47d4a8724a8692907b1651dc156c0e7f5280e30d
Alexander Urrutia <matias04041994@gmail.com>
2024-02-09 21:22:16 -0300

    feat: :art: Se agregan props
    Se agrega una prop de custom style para customizar el boton a medida

--- src/components/molecules/Buttons/DefaultButton.tsx
+++ src/components/molecules/Buttons/DefaultButton.tsx
@@ -4,6 +4,7 @@ type Props = {
   backgroundColor: string
   hoverBackgroundColor: string
   url: string
+  customStyle?: object | undefined
 }
 
 import ArrowIcon from '@/assets/icons/arrow-right-bold.svg'
@@ -15,12 +16,14 @@ const DefaultButton = ({
   backgroundColor = '#E8732C',
   hoverBackgroundColor = '#009145',
   url = '',
+  customStyle,
 }: Props) => {
   return (
     <Link
-      className={`flex w-fit cursor-pointer flex-row items-center rounded-none border border-white bg-[${backgroundColor}] p-0 text-center font-bold text-white hover:bg-[${hoverBackgroundColor}] transition-all duration-700`}
+      className={`flex cursor-pointer flex-row items-center rounded-none border border-white bg-[${backgroundColor}] p-0 text-center font-bold text-white hover:bg-[${hoverBackgroundColor}] transition-all duration-700`}
       to={url}
       unstable_viewTransition
+      style={customStyle && customStyle}
     >
       <span className='border-r border-white px-4 py-2 text-sm font-normal'>
         {textFirst}
@@ -30,7 +33,7 @@ const DefaultButton = ({
       <img
         src={ArrowIcon}
         alt={`Icono de flecha que lleva a ${textFirst} ${textFirst} ${textSecond}`}
-        className='size-8 '
+        className='mx-2 size-8'
       />
     </Link>
   )
