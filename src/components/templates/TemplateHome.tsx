177acfaef31f99ffd4c820eebc20cbbc752a3ef1
Alexander Urrutia <matias04041994@gmail.com>
2024-02-11 12:10:16 -0300

    Se ocupa componente modular en template

--- src/components/templates/TemplateHome.tsx
+++ src/components/templates/TemplateHome.tsx
@@ -1,3 +1,6 @@
+import VideoMP4 from '@/assets/videos/home.mp4'
+import VideoWebm from '@/assets/videos/home.webm'
+import VideoBanner from '@/components/molecules/VideoBanner'
 import Footer from '@/components/organism/Footer'
 import AporteRegional from '@/components/organism/Home/AporteRegional'
 import Compromisos from '@/components/organism/Home/Compromisos'
@@ -5,7 +8,6 @@ import EtapasProyecto from '@/components/organism/Home/EtapasProyecto'
 import Noticias from '@/components/organism/Home/Noticias'
 import PreguntasFrecuentes from '@/components/organism/Home/PreguntasFrecuentes'
 import PresentacionUbicacion from '@/components/organism/Home/PresentacionUbicacion'
-import PrincipalSection from '@/components/organism/Home/PrincipalSection'
 import Navbar from '@/components/organism/MenuNav'
 import { Helmet } from 'react-helmet-async'
 
@@ -23,7 +25,22 @@ const TemplateHome = () => {
       </Helmet>
       <Navbar />
       <main>
-        <PrincipalSection />
+        <VideoBanner
+          videoMP4={VideoMP4}
+          videoWEBM={VideoWebm}
+          bgMobile='/assets/images/posters/poster-video-home-mobile.webp'
+          title={
+            <h1 className='text-center text-5xl font-bold text-white lg:text-balance lg:text-6xl'>
+              Proyecto
+              <span className='text-[#E8732D]'> Vizcachitas</span>
+            </h1>
+          }
+          description='Minería sostenible y responsable comprometida con el desarrollo
+              social y económico de Putaendo, San Felipe y la Región de
+              Valparaíso. Desarrollo con las comunidades y cuidado del medio
+              ambiente para un futuro mejor.'
+          list={true}
+        />
         <PresentacionUbicacion />
         <Compromisos />
         <AporteRegional />
