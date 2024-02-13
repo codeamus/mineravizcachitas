f76061b2c644a1dbe426fe96af1fd251153eb707
Alexander Urrutia <matias04041994@gmail.com>
2024-02-13 16:35:24 -0300

    Ajustar hover cards para aporte regional home

--- src/components/molecules/Cards/RegionalCard.tsx
+++ src/components/molecules/Cards/RegionalCard.tsx
@@ -7,15 +7,15 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const RegionalCard = ({ title, description, image }: Props) => {
   return (
-    <article className='article duration-400 relative w-full  max-w-[320px] transition-all hover:shadow-xl hover:shadow-zinc-700'>
+    <article className='article duration-400 relative w-full  max-w-[320px] transition-all hover:shadow-xl hover:shadow-zinc-700 [&_div]:hover:opacity-100 [&_picture_img]:hover:brightness-[0.40]'>
       <picture>
         <LazyLoadImage
           src={image}
-          className='article-hover:brightness-100  duration-400 aspect-[3/4] brightness-[0.60] transition-all'
+          className='duration-400 aspect-[3/4] transition-all'
           alt={`Imagen representativa de ${title}`}
         />
       </picture>
-      <div className='absolute top-0 flex h-full w-full flex-col justify-between p-4'>
+      <div className='absolute top-0 flex h-full w-full flex-col justify-between p-4 opacity-0 transition-opacity duration-500'>
         <h3 className='min-h-[75px] whitespace-pre border-b border-white pb-2 text-center text-2xl font-bold uppercase text-white'>
           {title}
         </h3>
