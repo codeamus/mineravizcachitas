415988fef4f81b5cf8fdd702f78391aabbbaea3f
Alexander Urrutia <matias04041994@gmail.com>
2024-02-14 12:15:03 -0300

    Ajustes de margen para descripcion

--- src/components/molecules/VideoBanner/index.tsx
+++ src/components/molecules/VideoBanner/index.tsx
@@ -48,7 +48,7 @@ const VideoBanner = ({
         <div className='flex flex-col items-center gap-10 lg:flex-row lg:gap-20'>
           {title}
           <div>
-            <p className='text-pretty border-b border-t border-[#E8732D] py-4 text-center text-sm leading-normal text-white lg:text-start lg:text-lg lg:leading-10'>
+            <p className='my-4 text-pretty border-b border-t border-[#E8732D] py-4 text-center text-sm leading-normal text-white lg:my-0 lg:text-start lg:text-lg lg:leading-10'>
               {description}
             </p>
           </div>
