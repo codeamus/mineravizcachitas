4538ab63adec66428c11f7da483e20ae4803ddb8
Alexander Urrutia <matias04041994@gmail.com>
2024-02-22 16:03:06 -0300

    icons beneficios

--- src/const/beneficios.ts
+++ src/const/beneficios.ts
@@ -6,16 +6,21 @@ interface BENEFICIO {
   icon: string
 }
 
-import IconPrefactibilidad from '@/assets/icons/icon-prefactibilidad.svg'
+import IconEconomia from '@/assets/icons/icon-economia.svg'
+import IconEmpleo from '@/assets/icons/icon-empleo.svg'
+import IconVida from '@/assets/icons/icon-vida.svg'
+import IconConectividad from '@/assets/icons/icon-conectividad.svg'
+import IconAmbiente from '@/assets/icons/icon-ambiente-etapas.svg'
+import IconEducacion from '@/assets/icons/icon-educacion.svg'
 
 const BENEFICIOS_LOCALES: BENEFICIO[] = [
   {
-    titleTab: 'ECONOMÍA LOCAL',
+    titleTab: 'Economía local',
     titleContent: 'UN APORTE A LA',
     titleContentStrong: 'ECONOMÍA DE PUTAENDO',
     content:
       'El proyecto minero Vizcachitas, con una duración de al menos 26 años, es una oportunidad de desarrollo para la V Región, la Provincia de San Felipe y especialmente para la comuna de Putaendo, que tiene una alta tasa de pobreza multidimensional según la Encuesta CASEN de 2017. Estamos trabajando en la preparación del capital humano necesario para la industria minera y colaborando con la comunidad en iniciativas de capacitación y emprendimiento, para que mujeres y jóvenes puedan generar sus propios ingresos y contribuir al proyecto minero y sus servicios asociados.',
-    icon: IconPrefactibilidad,
+    icon: IconEconomia,
   },
   {
     titleTab: 'Empleos',
@@ -23,48 +28,48 @@ const BENEFICIOS_LOCALES: BENEFICIO[] = [
     titleContentStrong: 'CALIDAD',
     content:
       'El proyecto Vizcachitas destaca como una inversión minera clave en Chile, con una inversión de más de 2.800 millones de dólares. Durante la fase de construcción, se crearán 5.500 empleos, y una vez en operación, proporcionará 1.200 puestos de trabajo directos, además de empleos indirectos a través de cadenas productivas asociadas. Buscamos que los residentes de Putaendo y la provincia accedan a empleos de calidad y participen en la planificación de su futuro. La empresa está comprometida en la preparación del capital humano necesario para la industria minera, asegurando que técnicos y profesionales locales estén listos para contribuir al proyecto.',
-    icon: IconPrefactibilidad,
+    icon: IconEmpleo,
   },
   {
-    titleTab: 'CALIDAD DE VIDA',
+    titleTab: 'Calidad de vida',
     titleContent: 'UNA VIDA',
     titleContentStrong: 'MEJOR',
     content: `Putaendo y sus alrededores merecen la oportunidad de una vida mejor, merecen proyectos concretos y no promesas, merecen ser tratados con respeto y valoración de sus capacidades y ganas de aprender y surgir.
     Vizcachitas no es para unos pocos, es una invitación para crecer sustentablemente, con minería verde, de clase mundial y abierta a la comunidad, a la región y al Chile del futuro.
     Es un compromiso con el cuidado del entorno, de sus recursos hídricos, la flora y fauna, las tradiciones culturales y la tranquilidad de pensar que existe un mañana mejor para hombres, mujeres y jóvenes de esta zona.`,
-    icon: IconPrefactibilidad,
+    icon: IconVida,
   },
   {
-    titleTab: 'CONECTIVIDAD',
+    titleTab: 'Conectividad',
     titleContent: 'MÁS CONECTIVIDAD',
     titleContentStrong: 'Y SEGURIDAD',
     content: `El concentrado de cobre que se extraiga del yacimiento será transportado en camiones, utilizando un sistema de rotainers o contenedores sellados de volteo, que tienen como ventaja principal que todo el proceso (transporte, acopio y embarque) se haga sin emisiones al ambiente.
     Para aumentar los estándares de seguridad y mejor conectividad, se mejorarán 35 kilómetros de vías existentes entre Vizcachitas y Putaendo, minimizando la contaminación por material particulado en suspensión (polvo).
     `,
-    icon: IconPrefactibilidad,
+    icon: IconConectividad,
   },
   {
-    titleTab: 'MEDIO AMBIENTE',
+    titleTab: 'Medio ambiente',
     titleContent: 'ESTÁNDARES',
     titleContentStrong: 'MUNDIALES',
     content: `Vizcachitas es un proyecto minero de clase mundial, porque se ha incorporado tecnología y estándares ambientales de última generación. 
     Es una nueva forma de hacer minería, más verde, de futuro, comprometida con una producción que reduce impactos ambientales y genera externalidades positivas sostenidas en el tiempo en su entorno.
     De esta manera, y escuchando a la comunidad, se utilizará agua desalinizada en su operación, no habrá tranque de relave (sólo depósitos de relave) y se protegerá la flora y fauna del sector.`,
-    icon: IconPrefactibilidad,
+    icon: IconAmbiente,
   },
   {
-    titleTab: 'SALUD',
+    titleTab: 'Salud',
     titleContent: 'Lorem ipsum',
     titleContentStrong: 'lorem',
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem tenetur amet deserunt quia molestiae voluptates inventore tempora nulla sunt mollitia aut ad fugit vel accusantium fugiat earum ullam, iure nesciunt.`,
-    icon: IconPrefactibilidad,
+    icon: IconEducacion,
   },
   {
-    titleTab: 'EDUCACIÓN',
+    titleTab: 'Educación',
     titleContent: 'APRENDER ',
     titleContentStrong: 'PARA CRECER',
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem tenetur amet deserunt quia molestiae voluptates inventore tempora nulla sunt mollitia aut ad fugit vel accusantium fugiat earum ullam, iure nesciunt.`,
-    icon: IconPrefactibilidad,
+    icon: IconEducacion,
   },
 ]
 
