21737a5f4de5f22780c4d0af6a60c3f0fadaeb68
Alexander Urrutia <matias04041994@gmail.com>
2024-02-22 09:33:43 -0300

    Pasar de Google fonts a Paquete de fuentes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import Route from './routes'
+import '@fontsource-variable/montserrat'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
