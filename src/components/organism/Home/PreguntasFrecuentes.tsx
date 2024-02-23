061be661669c7a08ccd6181ff7912435d2542e48
Alexander Urrutia <matias04041994@gmail.com>
2024-02-23 18:07:48 -0300

    Fix accordion

--- src/components/organism/Home/PreguntasFrecuentes.tsx
+++ src/components/organism/Home/PreguntasFrecuentes.tsx
@@ -7,7 +7,7 @@ const PreguntasFrecuentes = () => {
   return (
     <section
       id='preguntas'
-      className='mb-12 flex flex-col items-center justify-center gap-10 p-4 md:p-8 lg:flex-row'
+      className='mb-12 flex flex-col items-start justify-center gap-10 p-4 md:p-8 lg:flex-row'
     >
       <div className='flex w-full flex-col items-center justify-center lg:w-1/2 lg:items-end lg:justify-end'>
         <h2 className='text-balance text-center text-4xl font-bold text-black lg:text-end'>
@@ -38,22 +38,29 @@ const PreguntasFrecuentes = () => {
         </h3>
         <div>
           {FAQS_HOME.map(({ title, content }, index) => (
-            <div
-              className='collapse collapse-plus mt-4 rounded-none border border-[#E8732D] bg-white'
-              key={index}
-            >
-              <input
-                type='radio'
-                name='accordion'
-                aria-label={`Input de accordio para la pregunta ${title}`}
-              />
-              <div className='collapse-title text-sm font-bold text-black after:text-3xl after:font-normal after:text-[#E8732D]'>
-                {title}
-              </div>
-              <div className='collapse-content border-t border-[#E8732D] !p-0'>
-                <p className='p-3 text-sm text-black'>{content}</p>
-              </div>
-            </div>
+            <details className='group mt-4' key={index}>
+              <summary className='flex cursor-pointer list-none items-start justify-between rounded-none border border-[#E8732D] bg-white p-3'>
+                <span className='text-sm font-bold text-black'>{title}</span>
+                <span className='transition group-open:rotate-180'>
+                  <svg
+                    fill='none'
+                    height='24'
+                    shape-rendering='geometricPrecision'
+                    stroke='#000'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='1.5'
+                    viewBox='0 0 24 24'
+                    width='24'
+                  >
+                    <path d='M6 9l6 6 6-6'></path>
+                  </svg>
+                </span>
+              </summary>
+              <p className='group-open:animate-fadeIn border-b border-l border-r border-[#E8732D] p-3 text-sm text-black'>
+                {content}
+              </p>
+            </details>
           ))}
         </div>
       </div>
