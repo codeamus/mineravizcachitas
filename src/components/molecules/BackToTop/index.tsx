76a5cdac66d871cad89fa2aedc8a4fc3032ed7b8
Alexander Urrutia <matias04041994@gmail.com>
2024-02-24 13:57:12 -0300

    fix btn

--- src/components/molecules/BackToTop/index.tsx
+++ src/components/molecules/BackToTop/index.tsx
@@ -4,7 +4,7 @@ const BackToTop = () => {
   return (
     <a
       href='#top'
-      className='sticky w-fit bottom-10 z-50 flex items-center justify-center ml-auto transition-all duration-500'
+      className='sticky w-fit bottom-10 z-50 flex items-center justify-center ml-auto transition-all duration-500 h-0'
     >
       <LazyLoadImage
         className='hover:shadow-4xl relative right-5 cursor-pointer rounded-full bg-[#009145] p-2 shadow-slate-800 transition-all duration-500 hover:bg-[#E8732D]'
