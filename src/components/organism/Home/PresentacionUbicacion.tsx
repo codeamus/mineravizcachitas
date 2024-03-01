d4a6262828347428a27fd1cef472fca5fae44e38
Alexander Urrutia <matias04041994@gmail.com>
2024-03-01 09:33:24 -0300

    fix height iframe

--- src/components/organism/Home/PresentacionUbicacion.tsx
+++ src/components/organism/Home/PresentacionUbicacion.tsx
@@ -46,7 +46,7 @@ const PresentacionUbicacion = () => {
       <div className='flex flex-col-reverse bg-white lg:flex-row'>
         <iframe
           src='https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d8526.547228612466!2d-70.4233315!3d-32.4054278!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzLCsDI0JzE4LjkiUyA3MMKwMjUnMTguNyJX!5e1!3m2!1ses!2scl!4v1708614173019!5m2!1ses!2scl'
-          className='h-[300px] w-full lg:h-[560px] lg:w-1/2 aspect-[4/3] object-cover'
+          className='h-[300px] w-full lg:h-full lg:w-1/2 aspect-[4/3] object-cover'
           loading='lazy'
           referrerPolicy='no-referrer-when-downgrade'
           title='Iframe de Google Maps de la ubicación de Vizcachitas'
