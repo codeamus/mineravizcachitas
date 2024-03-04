2d501c548f48f61515aa075c6690e78f4fbeb800
Alexander Urrutia <matias04041994@gmail.com>
2024-03-04 18:24:03 -0300

    font size

--- src/components/organism/Home/DescubreProyecto.tsx
+++ src/components/organism/Home/DescubreProyecto.tsx
@@ -10,21 +10,21 @@ const DescubreProyecto = () => {
           <article>
                <h3 className="font-bold text-[#009145] text-center text-balance border-b border-[#009145]    mb-4 py-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
                <iframe className="w-full h-[300px]" src="https://www.youtube.com/embed/aa73LUEtyDA?si=jb_kHe9mZf0F-Kfb" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-               <p className="text-black text-center mt-4">
+               <p className="text-black text-center mt-4 text-sm">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident expedita, sunt quia laudantium repellendus ut eaque. Voluptatibus, sed nemo rerum suscipit pariatur porro, beatae perferendis, ducimus reiciendis officiis nesciunt nobis?
                </p>
           </article>
           <article>
                <h3 className="font-bold text-[#009145] text-center text-balance border-b border-[#009145]    mb-4 py-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
                <iframe className="w-full h-[300px]" src="https://www.youtube.com/embed/aa73LUEtyDA?si=jb_kHe9mZf0F-Kfb" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-               <p className="text-black text-center mt-4">
+               <p className="text-black text-center mt-4 text-sm">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident expedita, sunt quia laudantium repellendus ut eaque. Voluptatibus, sed nemo rerum suscipit pariatur porro, beatae perferendis, ducimus reiciendis officiis nesciunt nobis?
                </p>
           </article>
           <article>
                <h3 className="font-bold text-[#009145] text-center text-balance border-b border-[#009145]    mb-4 py-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
                <iframe className="w-full h-[300px]" src="https://www.youtube.com/embed/aa73LUEtyDA?si=jb_kHe9mZf0F-Kfb" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-               <p className="text-black text-center mt-4">
+               <p className="text-black text-center mt-4 text-sm">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident expedita, sunt quia laudantium repellendus ut eaque. Voluptatibus, sed nemo rerum suscipit pariatur porro, beatae perferendis, ducimus reiciendis officiis nesciunt nobis?
                </p>
           </article>
