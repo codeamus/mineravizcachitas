008d32f1c98a1b1e1b3e8048205a61412b46bb30
Alexander Urrutia <matias04041994@gmail.com>
2024-03-29 15:38:42 -0300

    Fix atributos

--- src/components/organism/PreguntasFrecuentes/TodasPreguntas.tsx
+++ src/components/organism/PreguntasFrecuentes/TodasPreguntas.tsx
@@ -15,11 +15,11 @@ const TodasPreguntas = () => {
                 <svg
                   fill='none'
                   height='24'
-                  shape-rendering='geometricPrecision'
+                  shapeRendering='geometricPrecision'
                   stroke='#000'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='1.5'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='1.5'
                   viewBox='0 0 24 24'
                   width='24'
                 >
--- src/components/organism/Sostenibilidad/Compromisos.tsx
+++ src/components/organism/Sostenibilidad/Compromisos.tsx
@@ -56,11 +56,11 @@ const ComponentCompromisos = ({
                 <svg
                   fill='none'
                   height='24'
-                  shape-rendering='geometricPrecision'
+                  shapeRendering='geometricPrecision'
                   stroke='#000'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='1.5'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='1.5'
                   viewBox='0 0 24 24'
                   width='24'
                 >
