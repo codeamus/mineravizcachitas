c1295a86f0034e4c21e3fa92359a4f0eff1a8a54
Alexander Urrutia <matias04041994@gmail.com>
2024-04-01 19:35:19 -0300

    Nuevos types creados

---
+++ src/types/HomeData.ts
@@ -0,0 +1,14 @@
+export interface HomeData {
+  first_title: string
+  first_description: string
+  image: string
+  second_title: string
+  second_description: string
+  link_google_maps: string
+}
+
+export interface VideosDescubre {
+  id_video_youtube: string
+  title: string
+  description: string
+}
