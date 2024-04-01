af5c7d916ca7d2933592f93ff4ef547a162942a9
Alexander Urrutia <matias04041994@gmail.com>
2024-04-01 20:02:03 -0300

    fix types

--- src/types/HomeData.ts
+++ src/types/HomeData.ts
@@ -7,7 +7,7 @@ export interface HomeData {
   dataPresentacion?: {
     first_title: string
     first_description: string
-    image: string
+    image: { url: string }
     second_title: string
     second_description: string
     link_google_maps: string
