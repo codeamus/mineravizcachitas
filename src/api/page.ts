ffc930828e874fd080cb6d75fe821ed38acbad05
Alexander Urrutia <matias04041994@gmail.com>
2024-04-01 19:35:07 -0300

    Se arregla api url fetch

--- src/api/page.ts
+++ src/api/page.ts
@@ -1,8 +1,8 @@
-
-
-const getDataPage = async (slugPage : string) => {
+const getDataPage = async (slugPage: string) => {
   const response = await fetch(
-    `${import.meta.env.VITE_BASE_API_URL}/pages?slug=${slugPage}&acf_format=standard`
+    `${
+      import.meta.env.VITE_BASE_API_URL
+    }pages?_fields=acf&slug=${slugPage}&acf_format=standard`
   )
   const news = await response.json()
   return news
