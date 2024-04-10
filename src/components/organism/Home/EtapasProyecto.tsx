af8a83939375b766d7953a29593ce2c6e7244701
Alexander Urrutia <matias04041994@gmail.com>
2024-04-10 17:27:05 -0400

    Ajustes

--- src/components/organism/Home/EtapasProyecto.tsx
+++ src/components/organism/Home/EtapasProyecto.tsx
@@ -6,60 +6,64 @@ import * as Tabs from '@radix-ui/react-tabs'
 const EtapasProyecto = ({ dataEtapas }: HomeData) => {
   return (
     <section
-      id='etapas'
-      className='relative min-h-[820px] bg-cover bg-no-repeat px-4 py-10 lg:min-h-0 lg:bg-fixed lg:py-20 xl:px-20'
+      id="etapas"
+      className="relative min-h-[820px] bg-cover bg-no-repeat px-4 py-10 lg:min-h-0 lg:bg-fixed lg:py-20 xl:px-20"
       style={{
         backgroundImage: `${
           window.innerWidth < 768
-            ? 'url(/assets/images/backgrounds/bg-etapas-mobile.webp)'
-            : 'url(/assets/images/backgrounds/bg-etapas.webp)'
+            ? `url(${
+                import.meta.env.BASE_URL
+              }/assets/images/backgrounds/bg-etapas-mobile.webp)`
+            : `url(${
+                import.meta.env.BASE_URL
+              }/assets/images/backgrounds/bg-etapas.webp)`
         }`,
       }}
     >
-      <BtnScrollDown section='etapas' />
-      <h2 className='text-center text-4xl font-bold text-white'>
+      <BtnScrollDown section="etapas" />
+      <h2 className="text-center text-4xl font-bold text-white">
         Etapas del proyecto
       </h2>
-      <div className='mt-8 flex items-center justify-center lg:mt-14'>
-        <Tabs.Root defaultValue='tab0'>
-          <div className='lg:mb-10'>
+      <div className="mt-8 flex items-center justify-center lg:mt-14">
+        <Tabs.Root defaultValue="tab0">
+          <div className="lg:mb-10">
             <Tabs.List
-              className='flex justify-center gap-2 pb-2 '
-              aria-label='Etapas del proyecto'
+              className="flex justify-center gap-2 pb-2 "
+              aria-label="Etapas del proyecto"
             >
               {dataEtapas?.map((etapa, index) => (
                 <Tabs.Trigger
-                  className='tab w-fit max-w-2xl flex h-full items-center mx-3 lg:mx-10 flex-col leading-[15px] text-white focus:outline-[0px] focus:outline-offset-0 transition-all duration-300 [&_img]:hover:scale-110 [&_span]:hover:bg-[#e8732d] [&_span]:hover:border-[#e8732d]'
+                  className="tab w-fit max-w-2xl flex h-full items-center mx-3 lg:mx-10 flex-col leading-[15px] text-white focus:outline-[0px] focus:outline-offset-0 transition-all duration-300 [&_img]:hover:scale-110 [&_span]:hover:bg-[#e8732d] [&_span]:hover:border-[#e8732d]"
                   value={`tab${index}`}
                   key={index}
                 >
                   <img
                     src={etapa.icon.url}
-                    className='size-12 transition-all duration-500 lg:size-16'
+                    className="size-12 transition-all duration-500 lg:size-16"
                     alt={`Icono relacionado a la etapa ${etapa.title_nav}`}
                   />
-                  <span className='mt-3 hidden border-b border-t border-white p-2 text-white transition-all duration-500 ease-in-out lg:block lg:max-w-[180px]'>
+                  <span className="mt-3 hidden border-b border-t border-white p-2 text-white transition-all duration-500 ease-in-out lg:block lg:max-w-[180px]">
                     {etapa.title_nav}
                   </span>
                 </Tabs.Trigger>
               ))}
             </Tabs.List>
           </div>
-          <div className='w-full flex gap-10'>
+          <div className="w-full flex gap-10">
             {dataEtapas?.map((etapa, index) => (
               <Tabs.Content
-                className='grow rounded-b-md p-5 outline-none'
+                className="grow rounded-b-md p-5 outline-none"
                 value={`tab${index}`}
                 key={index}
               >
-                <div className='flex w-fit flex-col items-center gap-8 xl:flex-row'>
-                  <div className='text-center xl:text-start'>
+                <div className="flex w-fit flex-col items-center gap-8 xl:flex-row">
+                  <div className="text-center xl:text-start">
                     {etapa.title_content && (
-                      <h3 className='mb-4 text-2xl lg:text-3xl font-bold text-white lg:text-end'>
+                      <h3 className="mb-4 text-2xl lg:text-3xl font-bold text-white lg:text-end">
                         {etapa.title_content}
                       </h3>
                     )}
-                    <p className='text-pretty text-sm lg:text-end leading-loose text-white'>
+                    <p className="text-pretty text-sm lg:text-end leading-loose text-white">
                       {etapa.content}
                     </p>
                   </div>
@@ -67,9 +71,9 @@ const EtapasProyecto = ({ dataEtapas }: HomeData) => {
               </Tabs.Content>
             ))}
             <img
-              className='hidden  xl:block aspect-[661/606] object-cover'
+              className="hidden  xl:block aspect-[661/606] object-cover"
               src={ImageEtapas}
-              alt='Imagen de la sección etapas'
+              alt="Imagen de la sección etapas"
             />
           </div>
         </Tabs.Root>
--- src/components/organism/MenuNav/index.tsx
+++ src/components/organism/MenuNav/index.tsx
@@ -11,64 +11,64 @@ import { Link, NavLink } from 'react-router-dom'
 const Navbar = ({ position = 'absolute', bgColor }: MenuNavProps) => {
   return (
     <header
-      id='header-nav'
+      id="header-nav"
       className={`${position} top-0 z-20 w-full ${bgColor && bgColor} px-10`}
     >
       <div
-        id='headerBox'
-        className='mx-auto flex items-center justify-between gap-16 border-b border-none border-white lg:border-solid'
+        id="headerBox"
+        className="mx-auto flex items-center justify-between gap-16 border-b border-none border-white lg:border-solid"
       >
         <a
-          href='#header-menu'
-          className='md:hidden'
-          aria-label='Icono para abrir menu'
-          aria-labelledby='Icono de menu'
+          href="#header-menu"
+          className="md:hidden"
+          aria-label="Icono para abrir menu"
+          aria-labelledby="Icono de menu"
         >
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-8 w-8'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='#fff'
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="#fff"
           >
             <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M4 6h16M4 12h8m-8 6h16'
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
             />
           </svg>
         </a>
         <Link
-          to='/'
-          className='relative z-10 flex flex-row items-center'
+          to="/"
+          className="relative z-10 flex flex-row items-center"
           unstable_viewTransition
-          aria-label='Logo de Minera Vizcachitas que lleva a la home'
+          aria-label="Logo de Minera Vizcachitas que lleva a la home"
         >
           <img
-            className='size-24 transition-all hover:scale-110'
-            src='/assets/logos/logo.svg'
-            alt='Logo de Minera Vizcachitas'
+            className="size-24 transition-all hover:scale-110"
+            src={`${import.meta.env.BASE_URL}/assets/logos/logo.svg`}
+            alt="Logo de Minera Vizcachitas"
           />
-          <hr className='mx-4 hidden h-[60px] w-[1px] bg-white text-lg xl:block' />
-          <span className='hidden text-lg font-medium text-white xl:block'>
+          <hr className="mx-4 hidden h-[60px] w-[1px] bg-white text-lg xl:block" />
+          <span className="hidden text-lg font-medium text-white xl:block">
             Proyecto <br aria-hidden />
             Vizcachitas
           </span>
         </Link>
         <nav
-          id='header-menu'
-          className='fixed inset-0 z-20 flex h-dvh w-full -translate-y-full flex-col items-center justify-center gap-8 bg-[#000]/90 text-2xl transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:translate-y-[initial] md:flex-row md:bg-[initial] md:text-xl'
+          id="header-menu"
+          className="fixed inset-0 z-20 flex h-dvh w-full -translate-y-full flex-col items-center justify-center gap-8 bg-[#000]/90 text-2xl transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:translate-y-[initial] md:flex-row md:bg-[initial] md:text-xl"
         >
           <a
-            href='#header-nav'
-            className='absolute left-10 top-10 text-white md:hidden'
+            href="#header-nav"
+            className="absolute left-10 top-10 text-white md:hidden"
           >
             X
           </a>
-          <ul className='flex w-full flex-col items-center justify-center gap-10 md:hidden md:flex-row lg:flex'>
+          <ul className="flex w-full flex-col items-center justify-center gap-10 md:hidden md:flex-row lg:flex">
             {MENU.map(({ title, path }, index) => (
-              <li className='relative' key={index}>
+              <li className="relative" key={index}>
                 <NavLink
                   className={({ isActive }) =>
                     isActive
@@ -84,29 +84,29 @@ const Navbar = ({ position = 'absolute', bgColor }: MenuNavProps) => {
             ))}
           </ul>
         </nav>
-        <div className='flex flex-row gap-4'>
+        <div className="flex flex-row gap-4">
           <a
-            href='https://www.instagram.com/minera_vizcachitas/'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='Link que te dirige al perfil de Instagram de Minera Vizcachitas'
+            href="https://www.instagram.com/minera_vizcachitas/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Link que te dirige al perfil de Instagram de Minera Vizcachitas"
           >
             <img
-              className='w-8 sepia-0 transition-all hover:scale-125 hover:sepia'
+              className="w-8 sepia-0 transition-all hover:scale-125 hover:sepia"
               src={InstaIcon}
-              alt='Icono de Instagram'
+              alt="Icono de Instagram"
             />
           </a>
           <a
-            href='https://web.facebook.com/MineraVizcachitas/'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='Link que te dirige al perfil de Facebook de Minera Vizcachitas'
+            href="https://web.facebook.com/MineraVizcachitas/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Link que te dirige al perfil de Facebook de Minera Vizcachitas"
           >
             <img
-              className='w-8 sepia-0 transition-all hover:scale-125 hover:sepia'
+              className="w-8 sepia-0 transition-all hover:scale-125 hover:sepia"
               src={FBIcon}
-              alt='Icono de Facebook'
+              alt="Icono de Facebook"
             />
           </a>
         </div>
