9c52b19361bba8da8fffa9c2f299f4cea865745d
Alexander Urrutia <matias04041994@gmail.com>
2024-04-22 13:51:55 -0400

    ajustes quienes somos

--- src/components/organism/Somos/Equipo.tsx
+++ src/components/organism/Somos/Equipo.tsx
@@ -3,11 +3,11 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const Equipo = ({ dataTeam }: QuienesSomosType) => {
   return (
-    <section className='md:px-26 relative -top-[25px] flex flex-col items-center justify-center bg-[#F0EFEF] px-10 py-20 lg:px-36'>
-      <h2 className='mb-10 w-fit border-b-2 border-[#707070] text-center text-3xl font-bold uppercase text-[#8B8B8B] lg:text-4xl'>
+    <section className="md:px-26 relative -top-[25px] flex flex-col items-center justify-center bg-[#F0EFEF] px-10 py-20 lg:px-36">
+      <h2 className="mb-10 w-fit border-b-2 border-[#707070] text-center text-3xl font-bold uppercase text-[#8B8B8B] lg:text-4xl">
         Equipo corporativo
       </h2>
-      <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4'>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
         {dataTeam?.map((person: TeamType, index: number) => (
           <article key={index}>
             <picture>
@@ -16,11 +16,13 @@ const Equipo = ({ dataTeam }: QuienesSomosType) => {
                 alt={`Imagen de perfil para ${person.name}`}
               />
             </picture>
-            <h3 className='mt-2 text-lg font-bold text-[#8B8B8B]'>
+            <h3 className="mt-2 text-lg font-bold text-[#8B8B8B] min-h-[56px]">
               {person.name}
             </h3>
-            <p className='mb-6 mt-2 text-sm text-[#8B8B8B]'>{person.job}</p>
-            <span className='text-sm font-bold text-[#8B8B8B]'>
+            <p className="mb-6 mt-2 text-sm text-[#8B8B8B] min-h-[60px]">
+              {person.job}
+            </p>
+            <span className="text-sm font-bold text-[#8B8B8B]">
               {person.username}
             </span>
           </article>
